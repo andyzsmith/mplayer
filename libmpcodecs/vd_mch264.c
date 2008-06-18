a5d16b9fdbe308f1e7e7fa0cc05394d5b7277592
Andrew Wason <aw@motionbox.com>
2008-06-18 16:57:16 -0400

    Make deinterlacing conditional. Handle seeking. Fix compiler warnings.

--- libmpcodecs/vd_mch264.c
+++ libmpcodecs/vd_mch264.c
@@ -8,12 +8,16 @@
 
 #include <h264vdec.hpp>
 
+// If defined, use internal MainConcept deinterlacing.
+// Otherwise use interlaced frames and set proper fields on mp_image_t
+#undef DEINTERLACE
+
 static vd_info_t info = {
     "MainConcept H.264 Video",
     "mch264",
     "Motionbox",
     "Motionbox",
-    "Requires MainConcept low level H.264 Pro SDK"
+    "Requires MainConcept low level H.264 decoder SDK"
 };
 
 LIBVD_EXTERN(mch264)
@@ -24,17 +28,25 @@ typedef struct mch264_ctx {
 } mch264_ctx;
 
 // Set/get/query special features/parameters
-static int control(sh_video_t *sh, int cmd, void* arg, ...) {
+static int control(sh_video_t *sh, int cmd, void *arg __attribute__ ((unused)), ...) {
+    mch264_ctx *ctx = (mch264_ctx *)sh->context;
+    bufstream_tt *dec = ctx->dec;
+
     mp_msg(MSGT_DECVIDEO, MSGL_INFO, "mch264 control\n");//XXX
-    //XXX handle seek
-    return CONTROL_UNKNOWN;
+    switch (cmd) {
+    case VDCTRL_RESYNC_STREAM:
+        dec->auxinfo(dec, 0, PARSE_INIT, NULL, 0);
+        return CONTROL_TRUE;
+    default:
+        return CONTROL_UNKNOWN;
+    }
 }
 
 // Init driver
 static int init(sh_video_t *sh) {
     mch264_ctx *ctx;
     bufstream_tt *dec;
-
+    uint32_t parse_options = INTERN_REORDERING_FLAG;
     mp_msg(MSGT_DECVIDEO, MSGL_INFO, "mch264 init\n");//XXX
     
     //XXX use get_rc to provide message printing routines
@@ -49,15 +61,19 @@ static int init(sh_video_t *sh) {
     
     sh->context = ctx;
 
+#ifdef DEINTERLACE
+    parse_options |= DEINTERLACING_FLAG;
+#endif
+
     dec->auxinfo(dec, 0, PARSE_INIT, NULL, 0);
-    dec->auxinfo(dec, DEINTERLACING_FLAG|INTERN_REORDERING_FLAG, PARSE_OPTIONS, NULL, 0);
+    dec->auxinfo(dec, parse_options, PARSE_OPTIONS, NULL, 0);
     dec->auxinfo(dec, H264VD_SMP_AUTO, SET_SMP_MODE, NULL ,0);
     dec->auxinfo(dec, 2, SET_CPU_NUM, NULL, 0);
     dec->auxinfo(dec, H264VD_LF_STANDARD, SET_LOOP_FILTER_MODE, NULL, 0);
     dec->auxinfo(dec, SKIP_NONE, PARSE_FRAMES, NULL ,0);
 
     // Copy any extra data appended to bih
-    if (sh->bih && sh->bih->biSize > sizeof(BITMAPINFOHEADER))
+    if (sh->bih && (unsigned int)sh->bih->biSize > sizeof(BITMAPINFOHEADER))
         dec->copybytes(dec, (uint8_t *)(sh->bih+1), sh->bih->biSize - sizeof(BITMAPINFOHEADER));
 
     return 1;
@@ -74,6 +90,7 @@ static void uninit(sh_video_t *sh) {
     }
 }
 
+//XXX ifdef this out in production code
 static void dump_state(uint32_t state) {
     mp_msg(MSGT_DECVIDEO, MSGL_INFO, "mch264 state %x", state);
     if (state & PARSE_DONE_FLAG)
@@ -122,7 +139,7 @@ static mp_image_t *create_image(sh_video_t *sh) {
     struct SEQ_ParamsEx *seq_params = ctx->seq_params;
     uint32_t width;
     uint32_t height;
-    frame_tt frame = { 0 };
+    frame_tt frame = { .width = 0 };
     mp_image_t *mpi;
 
     if (!seq_params)
@@ -145,14 +162,25 @@ static mp_image_t *create_image(sh_video_t *sh) {
     frame.stride[2] = mpi->stride[1];
     frame.plane[2] = mpi->planes[1];
 
-    if (BS_OK == dec->auxinfo(dec, 0, GET_PIC, &frame, sizeof(frame)))
-        return mpi;   
+    if (BS_OK == dec->auxinfo(dec, 0, GET_PIC, &frame, sizeof(frame))) {
+        #ifndef DEINTERLACE
+        struct PIC_ParamsEx *pic_params;
+        if (BS_OK == dec->auxinfo(dec, 0, GET_PIC_PARAMSPEX, &pic_params, sizeof(pic_params))) {
+            mpi->pict_type = pic_params->picture_type;
+            mpi->fields = MP_IMGFIELD_ORDERED;
+            if (!pic_params->progressive_frame) mpi->fields |= MP_IMGFIELD_INTERLACED;
+            if (pic_params->top_field_first) mpi->fields |= MP_IMGFIELD_TOP_FIRST;
+            if (pic_params->repeat_first_field == 1) mpi->fields |= MP_IMGFIELD_REPEAT_FIRST;
+        }
+        #endif
+        return mpi;
+    }
 
     return NULL;
 }
 
 // Decode a frame
-static mp_image_t* decode(sh_video_t *sh, void *data, int length, int flags) {
+static mp_image_t* decode(sh_video_t *sh, void *data, int length, int flags __attribute__ ((unused))) {
     mch264_ctx *ctx = (mch264_ctx *)sh->context;
     bufstream_tt *dec = ctx->dec;
     mp_image_t *mpi = NULL;
@@ -179,8 +207,8 @@ static mp_image_t* decode(sh_video_t *sh, void *data, int length, int flags) {
                 if (BS_OK == dec->auxinfo(dec, 0, GET_SEQ_PARAMSPEX, &ctx->seq_params, sizeof(ctx->seq_params))) {
                     // These values appear to be display aspect ratio, not sample aspect ratio.
                     float dar = ctx->seq_params->aspect_ratio_width / (float)ctx->seq_params->aspect_ratio_height;
-                    uint32_t width = ctx->seq_params->horizontal_size;
-                    uint32_t height = ctx->seq_params->vertical_size;
+                    int width = ctx->seq_params->horizontal_size;
+                    int height = ctx->seq_params->vertical_size;
                     if (sh->aspect != dar || width != sh->disp_w || height != sh->disp_h) {
                         sh->aspect = dar;
                         mp_msg(MSGT_DECVIDEO, MSGL_INFO, "mch264 aspect %f\n", dar);
