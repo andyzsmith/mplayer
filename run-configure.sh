031061eddd3f1f49e2691be4228407bc662adfc7
Andrew Wason <aw@motionbox.com>
2008-06-27 18:05:35 -0400

    Move mch264dec section in configure. More specific run-configure.sh.
    Needed to move mch264dec to end since adding that framework without a -F
    causes other test compiles to fail.
    
    Changed run-configure.sh to explicitly enable/disable what we want
    and had to fix to explicitly list libs for some components that depend
    on autodetection to configure themselves (and we aren't autodetecting
    we are explicitly enabling).

--- run-configure.sh
+++ run-configure.sh
@@ -5,36 +5,78 @@
 # http://lists.mplayerhq.hu/pipermail/mplayer-dev-eng/2006-November/047633.html
 # http://wiki.multimedia.cx/index.php?title=MPlayer_FAQ#Build_questions.2Fcrashes
 
+# Since we don't autodetect XviD, faac, libdca etc. we must explicitly include them in --extra-libs
+
 MCROOT=/Users/aw/Projects/motionbox/encoder/rendermix/demo_codec_sdk_v7.5_release
 ./configure \
   --prefix=/Users/aw/Projects/software/encoding/installed \
   --codecsdir=/Users/aw/Projects/software/encoding/installed/codecs \
   --with-extralibdir=/Users/aw/Projects/software/encoding/installed/lib:/opt/local/lib \
   --with-extraincdir=/Users/aw/Projects/software/encoding/installed/include:/opt/local/include:"$MCROOT/include" \
-  --extra-libs=-F"$MCROOT/Frameworks" \
-  --disable-radio-v4l2 \
+  --extra-libs="-F$MCROOT/Frameworks -lxvidcore -lm -lfaac -ldts -lx264" \
+  --disable-alsa \
+  --disable-apple-ir \
+  --disable-apple-remote \
+  --disable-arts \
+  --disable-ass \
+  --disable-bitmap-font \
+  --disable-caca \
+  --disable-cddb \
+  --disable-libcdio \
+  --disable-cdparanoia \
+  --disable-dvdnav \
+  --disable-enca \
+  --disable-esd \
+  --disable-fontconfig \
+  --disable-freetype \
+  --disable-fribidi \
+  --disable-ftp \
+  --disable-jack \
+  --disable-live \
+  --disable-nas \
+  --disable-network \
+  --disable-openal \
+  --disable-ossaudio \
+  --disable-pulse \
+  --disable-pvr \
   --disable-radio-bsdbt848 \
+  --disable-radio-v4l2 \
+  --disable-sgiaudio \
+  --disable-smb \
+  --disable-sortsub \
+  --disable-sunaudio \
+  --disable-toolame \
   --disable-tv \
-  --disable-tv-v4l1 \
-  --disable-tv-v4l2 \
   --disable-tv-bsdbt848 \
   --disable-tv-teletext \
-  --disable-pvr \
+  --disable-tv-v4l1 \
+  --disable-tv-v4l2 \
   --disable-unrarexec \
-  --disable-ftp \
-  --disable-ass \
+  --disable-vidix \
+  --disable-win32waveout \
+  --disable-x11 \
+  --disable-xmms \
   --disable-xss \
-  --disable-bitmap-font \
-  --disable-freetype \
-  --disable-fontconfig \
-  --enable-mch264dec \
-  --enable-xmms \
-  --enable-gif \
-  --enable-png \
-  --enable-jpeg \
-  --enable-tremor-external \
-  --enable-theora \
+  --enable-faac \
   --enable-faad-external \
-  --disable-apple-remote \
-  --enable-debug \
+  --enable-gif \
+  --enable-jpeg \
+  --enable-largefiles \
+  --enable-libamr_nb \
+  --enable-libamr_wb \
+  --enable-libdca \
+  --enable-libdv \
+  --enable-liblzo \
+  --enable-mch264dec \
+  --enable-musepack \
+  --enable-png \
+  --enable-pthreads \
+  --enable-speex \
+  --enable-theora \
+  --enable-tremor-external \
+  --enable-twolame \
+  --enable-x264 \
+  --enable-xvid \
 ;
+#  --enable-yuv4mpeg \
+#  --enable-debug \
