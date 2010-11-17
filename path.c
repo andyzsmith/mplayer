andyzsmith/mplayer

commit cb134dfdaac01c79eda2fccccb47d60d0e7edd37

Export mp_basename in a function instead of duplicate macros in various places
git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@32630 b3059339-0415-0410-9bf9-f77b7e298cf2

path.c
@@ -193,3 +193,16 @@ void set_codec_path(const char *path)
     strcpy(codec_path, path);
     needs_free = 1;
 }
+
+const char *mp_basename(const char *path)
+{
+    char *s;
+
+#if HAVE_DOS_PATHS
+    s = strrchr(path, '\\');
+    if (s)
+        return s + 1;
+#endif
+    s = strrchr(path, '/');
+    return s ? s + 1 : s;
+}

gui/interface.c
@@ -1134,8 +1134,6 @@ void * gtkSet( int cmd,float fparam, void * vparam )
  return NULL;
 }
 
-#define mp_basename(s) (strrchr(s,'/')==NULL?(char*)s:(strrchr(s,'/')+1))
-
 #include "playtree.h"
 
 //This function adds/inserts one file into the gui playlist

libmenu/menu_pt.c
@@ -24,6 +24,7 @@
 #include "config.h"
 #include "mp_msg.h"
 #include "help_mp.h"
+#include "path.h"
 
 #include "libmpcodecs/img_format.h"
 #include "libmpcodecs/mp_image.h"
@@ -37,8 +38,6 @@
 #include "playtree.h"
 #include "input/input.h"
 #include "access_mpcontext.h"
-
-#define mp_basename(s) (strrchr((s),'/')==NULL?(char*)(s):(strrchr((s),'/')+1))
 
 struct list_entry_s {
   struct list_entry p;
@@ -156,7 +155,7 @@ static int op(menu_t* menu, char* args) {
   for( ; i != NULL ; i = i->next ) {
     e = calloc(1,sizeof(list_entry_t));
     if(i->files)
-      e->p.txt = mp_basename(i->files[0]);
+      e->p.txt = (char *)mp_basename(i->files[0]);
     else
       e->p.txt = "Group ...";
     e->pt = i;

mplayer.c
@@ -349,8 +349,6 @@ static int crash_debug = 0;
 #include "cfg-mplayer.h"
 
 
-#define mp_basename2(s) (strrchr(s,'/')==NULL?(char*)s:(strrchr(s,'/')+1))
-
 const void *mpctx_get_video_out(MPContext *mpctx)
 {
     return mpctx->video_out;
@@ -456,7 +454,7 @@ char *get_metadata (metadata_t type) {
   {
   case META_NAME:
   {
-    return strdup (mp_basename2 (filename));
+    return strdup(mp_basename(filename));
   }
 
   case META_VIDEO_CODEC:
@@ -1045,8 +1043,6 @@ static int libmpdemux_was_interrupted(int eof) {
   }
   return eof;
 }
-
-#define mp_basename(s) (strrchr(s,'\\')==NULL?(mp_basename2(s)):(strrchr(s,'\\')+1))
 
 static int playtree_add_playlist(play_tree_t* entry)
 {
@@ -3174,7 +3170,7 @@ while (player_idle_mode && !filename) {
 	mp_msg(MSGT_CPLAYER,MSGL_INFO,MSGTR_Playing,
 		filename_recode(filename));
         if(use_filename_title && vo_wintitle == NULL)
-            vo_wintitle = strdup ( mp_basename2 (filename));
+            vo_wintitle = strdup(mp_basename(filename));
     }
 
     edl_loadfile();
@@ -3368,7 +3364,8 @@ if (mpctx->demuxer && mpctx->demuxer->type==DEMUXER_TYPE_PLAYLIST)
   current_module="handle_demux_playlist";
   while (ds_get_packet(mpctx->demuxer->video,&playlist_entry)>0)
   {
-    char *temp, *bname;
+    char *temp;
+    const char *bname;
 
     mp_msg(MSGT_CPLAYER,MSGL_V,"Adding file %s to element entry.\n",
 	    filename_recode(playlist_entry));

path.h
@@ -26,5 +26,6 @@ extern char *codec_path;
 char *get_path(const char *filename);
 void set_path_env(void);
 void set_codec_path(const char *path);
+const char *mp_basename(const char *path);
 
 #endif /* MPLAYER_PATH_H */

commit 345fb9c965cd9e7b49cedf00390f0028a3cfaa1c

Fix crash on path without directories.
Regression introduced in r32630. Patch by Yuriy Kaminskiy yumkam at mail ru.



git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@32631 b3059339-0415-0410-9bf9-f77b7e298cf2

path.c
@@ -204,5 +204,5 @@ const char *mp_basename(const char *path)
         return s + 1;
 #endif
     s = strrchr(path, '/');
-    return s ? s + 1 : s;
+    return s ? s + 1 : path;
 }