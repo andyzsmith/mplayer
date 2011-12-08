andyzsmith/mplayer

commit f9f757c1ca33b6d4ba8b9e49e81fbb119d41bc02

Remove event evDropFile from Win32 GUI.
This event doesn't exist in the X11/GTK GUI
and it is identical to evLoadPlay.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@34403 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/win32/gui.c
@@ -238,7 +238,7 @@ static void handlemsg(HWND hWnd, int msg)
         case evLoadPlay:
         case evLoad:
             if(display_openfilewindow(gui, 0) && (msg == evLoadPlay))
-                handlemsg(hWnd, evDropFile);
+                handlemsg(hWnd, evLoadPlay);
             return;
         case evLoadSubtitle:
             display_opensubtitlewindow(gui);
@@ -501,13 +501,13 @@ static LRESULT CALLBACK SubProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
                         gui->playlist->add_track(gui->playlist, file, NULL, NULL, 0);
                 }
                 DragFinish((HDROP) wParam);
-                handlemsg(hWnd, evDropFile);
+                handlemsg(hWnd, evLoadPlay);
             }
             else
             {
                 gui->playlist->clear_playlist(gui->playlist);
                 gui->playlist->add_track(gui->playlist, (const char *) wParam, NULL, NULL, 0);
-                handlemsg(hWnd, evDropFile);
+                handlemsg(hWnd, evLoadPlay);
             }
             SetForegroundWindow(gui->subwindow);
             return 0;
@@ -790,13 +790,13 @@ static LRESULT CALLBACK EventProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
                         gui->playlist->add_track(gui->playlist, file, NULL, NULL, 0);
                 }
                 DragFinish((HDROP) wParam);
-                handlemsg(hWnd, evDropFile);
+                handlemsg(hWnd, evLoadPlay);
             }
             else
             {
                 gui->playlist->clear_playlist(gui->playlist);
                 gui->playlist->add_track(gui->playlist, (const char *) wParam, NULL, NULL, 0);
-                handlemsg(hWnd, evDropFile);
+                handlemsg(hWnd, evLoadPlay);
             }
             SetForegroundWindow(gui->mainwindow);
             return 0;
@@ -1118,7 +1118,7 @@ static LRESULT CALLBACK EventProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
 
 static void startplay(gui_t *gui)
 {
-    handlemsg(gui->mainwindow, evDropFile);
+    handlemsg(gui->mainwindow, evLoadPlay);
 }
 
 /* returns the bits per pixel of the desktop */

gui/win32/interface.c
@@ -227,7 +227,6 @@ static void guiSetEvent(int event)
             mp_input_queue_cmd(cmd);
             break;
         }
-        case evDropFile:
         case evLoadPlay:
         {
             switch(guiInfo.StreamType)

gui/win32/skinload.c
@@ -47,7 +47,6 @@ static const evName evNames[] =
 {
     {   evNone,                 "evNone"                },
     {   evPlay,                 "evPlay"                },
-    {   evDropFile,             "evDropFile"            },
     {   evStop,                 "evStop"                },
     {   evPause,                "evPause"               },
     {   evPrev,                 "evPrev"                },

gui/win32/skinload.h
@@ -183,7 +183,6 @@ skin_t *loadskin(char *skindir, int desktopbpp);
 #define evSetAspect         44
 #define evSetAudio          45
 #define evSetVideo          46
-#define evDropFile          47
 #define evPlayCD            48
 
 #define evExit              1000

commit 094ad327aa1236a5aa444dbe326f8e153017150f

Fix bug in Win32 GUI introduced in r34403.
When selecting a file to open in the open dialog, the open dialog just
reappears.

handlemsg() passes unknown messages (such as evDropFile) to the gui's
playercontrol function (guiSetEvent()). We cannot replace the
handlemsg(hWnd, evDropFile) call by handlemsg(hWnd, evLoadPlay), because
evLoadPlay is known in handlemsg() and has a different meaning there. So
we directly call gui->playercontrol(evLoadPlay) instead where evDropFile
and evLoadPlay have the same meaning.

Reported by Stephen Sheldon, sfsheldo gmail com.


git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@34413 b3059339-0415-0410-9bf9-f77b7e298cf2

gui/win32/gui.c
@@ -238,7 +238,7 @@ static void handlemsg(HWND hWnd, int msg)
         case evLoadPlay:
         case evLoad:
             if(display_openfilewindow(gui, 0) && (msg == evLoadPlay))
-                handlemsg(hWnd, evLoadPlay);
+                gui->playercontrol(evLoadPlay);
             return;
         case evLoadSubtitle:
             display_opensubtitlewindow(gui);
@@ -501,13 +501,13 @@ static LRESULT CALLBACK SubProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM l
                         gui->playlist->add_track(gui->playlist, file, NULL, NULL, 0);
                 }
                 DragFinish((HDROP) wParam);
-                handlemsg(hWnd, evLoadPlay);
+                gui->playercontrol(evLoadPlay);
             }
             else
             {
                 gui->playlist->clear_playlist(gui->playlist);
                 gui->playlist->add_track(gui->playlist, (const char *) wParam, NULL, NULL, 0);
-                handlemsg(hWnd, evLoadPlay);
+                gui->playercontrol(evLoadPlay);
             }
             SetForegroundWindow(gui->subwindow);
             return 0;
@@ -790,13 +790,13 @@ static LRESULT CALLBACK EventProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
                         gui->playlist->add_track(gui->playlist, file, NULL, NULL, 0);
                 }
                 DragFinish((HDROP) wParam);
-                handlemsg(hWnd, evLoadPlay);
+                gui->playercontrol(evLoadPlay);
             }
             else
             {
                 gui->playlist->clear_playlist(gui->playlist);
                 gui->playlist->add_track(gui->playlist, (const char *) wParam, NULL, NULL, 0);
-                handlemsg(hWnd, evLoadPlay);
+                gui->playercontrol(evLoadPlay);
             }
             SetForegroundWindow(gui->mainwindow);
             return 0;
@@ -1118,7 +1118,7 @@ static LRESULT CALLBACK EventProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
 
 static void startplay(gui_t *gui)
 {
-    handlemsg(gui->mainwindow, evLoadPlay);
+    gui->playercontrol(evLoadPlay);
 }
 
 /* returns the bits per pixel of the desktop */